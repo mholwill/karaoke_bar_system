e9523326119df89141d70d6d4e9ef85a934e01ba
Malcolm Holwill <malcolmholwill@Malcolms-workbook.local>
2020-01-24 14:41:43 +0000

    Initial Commit

---
+++ songs.rb
@@ -0,0 +1,10 @@
+class Songs
+
+def initialize(name)
+  @name = name
+end
+
+
+
+
+end
---
+++ specs/songs.specs.rb
@@ -0,0 +1,11 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../songs')
+
+class SongsTest < Minitest::Test
+
+
+
+end
